03c47b891bbe596361598bd6ad7dbf0b80a4119e
Luis <lprieto-@debian-11-inception.local>
2026-03-03 23:21:23 +0100

    > | port fix changes

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+set -eu
 
 cd /var/www/html
 
@@ -12,36 +12,8 @@ echo "MariaDB is up."
 
 chown -R www-data:www-data /var/www/html
 
-# Si no hay WordPress instalado, lo instalamos
-#if [ ! -f wp-config.php ] || [ ! -f wp-load.php ]; then
-#    echo "Downloading WordPress..."
-#    wp core download --allow-root
-
-#    echo "Creating wp-config..."
-#    wp config create --allow-root \
-#        --dbname="$MYSQL_DATABASE" \
-#        --dbuser="$MYSQL_USER" \
-#        --dbpass="$MYSQL_PASSWORD" \
-#        --dbhost="$MYSQL_HOST"
-
-#    echo "Installing WordPress..."
-#    wp core install --allow-root \
-#        --url="$DOMAIN_NAME" \
-#        --title="$WP_TITLE" \
-#        --admin_user="$WP_ADMIN" \
-#        --admin_password="$WP_ADMIN_PASSWORD" \
-#        --admin_email="$WP_ADMIN_EMAIL"
-
-#    echo "Creating normal user..."
-#    wp user create "$WP_USER" "$WP_USER_EMAIL" --allow-root \
-#        --user_pass="$WP_USER_PASSWORD" \
-#        --role=author || true
-#fi
-
-#wp option update siteurl "https://$DOMAIN_NAME" --allow-root
-#wp option update home "https://$DOMAIN_NAME" --allow-root
-
-if [ ! -f wp-config.php ]; then
+# Install WordPress if not installed
+if ! wp core is-installed --allow-root 2>/dev/null; then
     echo "Downloading WordPress..."
     wp core download --allow-root
 
@@ -52,8 +24,43 @@ if [ ! -f wp-config.php ]; then
         --dbpass="$MYSQL_PASSWORD" \
         --dbhost="$MYSQL_HOST"
 
-    echo "WordPress files ready. Installation will be done via browser."
+    echo "Installing WordPress..."
+    wp core install --allow-root \
+        --url="$DOMAIN_NAME" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL"
+
+    echo "Creating normal user..."
+    wp user create "$WP_USER" "$WP_USER_EMAIL" --allow-root \
+        --user_pass="$WP_USER_PASSWORD" \
+        --role=author || true
 fi
+if [ "$NGINX_PORT" = "443" ]; then
+    SITE_URL="https://$DOMAIN_NAME"
+else
+    SITE_URL="https://$DOMAIN_NAME:$NGINX_PORT"
+fi
+
+wp option update siteurl "$SITE_URL" --allow-root
+wp option update home "$SITE_URL" --allow-root
+#wp option update siteurl "https://$DOMAIN_NAME" --allow-root
+#wp option update home "https://$DOMAIN_NAME" --allow-root
+
+#if [ ! -f wp-config.php ]; then
+#    echo "Downloading WordPress..."
+#    wp core download --allow-root
+
+#    echo "Creating wp-config..."
+#    wp config create --allow-root \
+#        --dbname="$MYSQL_DATABASE" \
+#        --dbuser="$MYSQL_USER" \
+#        --dbpass="$MYSQL_PASSWORD" \
+#        --dbhost="$MYSQL_HOST"
+
+#    echo "WordPress files ready. Installation will be done via browser."
+#fi
 
 echo "Starting php-fpm..."
 mkdir -p /run/php
